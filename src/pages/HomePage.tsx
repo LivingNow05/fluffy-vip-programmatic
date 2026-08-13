b90edab50308fe89c8843514121158ac89fc9b29
Anthony <Livingnow05@gmail.com>
2026-08-12 23:52:52 -0500

    style: increase hero image width and apply negative margin offset

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -65,7 +65,7 @@ export const HomePage: React.FC<Props> = ({ cities, onOpenQuiz }) => {
           </div>
 
           {/* Columna Derecha (Visual Showcase) */}
-          <div className="w-full md:w-6/12 lg:w-6/12 flex-shrink-0 relative">
+          <div className="w-full md:w-8/12 lg:w-8/12 flex-shrink-0 relative md:-mt-12 lg:-mt-16 z-10">
             <div className="relative aspect-[3/2] w-full rounded-3xl overflow-hidden shadow-2xl group border-4 border-white dark:border-gray-800">
               <img
                 src="/images/fluffy-showcase-hero-light.jpg"
